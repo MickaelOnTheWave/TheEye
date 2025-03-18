faa5286e89f063ba57f615841ab473234ba990e6
Mickaël C. Guimarães <mickael.dacruz@gmail.com>
2025-03-17 21:09:49 -0300

    Added support for standard linux data folder

--- scripts/buildLinuxBinaries.sh
+++ scripts/buildLinuxBinaries.sh
@@ -5,5 +5,5 @@ source "BuildTools.sh"
 buildDir="../build/officialBuild"
 
 createFolderIfNotPresent $buildDir
-cmake -S ../  -B $buildDir -DCMAKE_BUILD_TYPE:STRING=Release
+cmake -S ../  -B $buildDir -DCMAKE_BUILD_TYPE:STRING=Release -DUSE_SYSTEM_DATA=ON
 cmake --build $buildDir
